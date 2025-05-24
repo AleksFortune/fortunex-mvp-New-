410d1778bd0de4ed5c4b6520fd39499923dccbb8
AleksFortune <alekspav65@gmail.com>
2025-05-25 01:19:49 +0300

    Add files via upload

---
+++ FortuneX MVP/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>FortuneX MVP</h1>
+      <p>Welcome to the FortuneX MVP deployed on GitHub Pages!</p>
+    </div>
+  )
+}
